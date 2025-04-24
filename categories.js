69b723da8e3261fa975d39261cb49c4d18241a5e
LINKEMIAO <163588310+LINKEMIAO@users.noreply.github.com>
2025-04-24 10:59:21 +0200

    header

---
+++ categories.js
@@ -0,0 +1,8 @@
+document.querySelector('.toggle-submenu').addEventListener('click', function () {
+    const secondNav = document.querySelector('.second-nav');
+    if (secondNav.style.display === 'none' || secondNav.style.display === '') {
+        secondNav.style.display = 'flex';
+    } else {
+        secondNav.style.display = 'none';
+    }
+});
