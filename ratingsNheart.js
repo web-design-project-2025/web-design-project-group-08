4f2b69c3c09ce3ad7bfd63cf09271154e734d164
Henketa <henrik6251@gmail.com>
2025-05-09 04:01:40 +0200

    comments

--- ratingsNheart.js
+++ ratingsNheart.js
@@ -1,9 +1,18 @@
 const stars = document.querySelectorAll(".stars i")
+const hearts = document.querySelectorAll(".heart-icon")
 
 stars.forEach((star, index1) => {
     star.addEventListener("click", () => {
         stars.forEach((star, index2) => {
         index1 >= index2 ? star.classList.add("Active") : star.classList.remove("Active")
+        });
+    });
+});
+
+hearts.forEach((icon) => {
+    icon.addEventListener("click", () => {
+        hearts.forEach((icon) => {
+        icon.classList.add("Active") ; icon.classList.remove("Active")
         });
     });
 });
